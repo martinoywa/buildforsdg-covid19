105a5e69b530b4096c3bbf76ae7af06298961c58
Martin Oywa <martinoywa2@gmail.com>
2020-04-13 11:09:57 +0300

    Add logging functionality

--- app.py
+++ app.py
@@ -1,35 +1,82 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify, Response, Request
 from src.estimator import estimator
 from dicttoxml import dicttoxml
 
+import time
+from flask import g
+
+import os
+from random import randint
+
+import requests
 
 app = Flask(__name__)
+
+app.config["LOGS"] = "logs/"
+
+if "logs" not in os.listdir():
+    os.mkdir(app.config["LOGS"])
+
+rand = randint(0, 1000000000)
+dir = os.path.join(app.config["LOGS"], "logs"+str(rand)+".txt")
+
+def logger(status):
+    now = time.time()
+    duration = int((time.time() - g.start) * 1000)
+    method = request.method
+    path = request.path
+
+    with open(dir, "a") as f:
+        f.write(f"{method}\t\t{path}\t\t{status}\t\t{duration}\n")
+
+    return dir
 
 
 @app.route("/api/v1/on-covid-19/", methods=["GET", "POST"])
 @app.route("/api/v1/on-covid-19/json", methods=["GET", "POST"])
 def json_index():
+    g.start = time.time()
+
     if request.method == "GET":
-        return jsonify("")
+        res = Response("", content_type="application/json")
+        status = res.status_code
+        logger(status)
+        return res
+
 
     if request.method == "POST":
         data = request.get_json()
         output = estimator(data)
-
+        res = Response(jsonify(output), content_type="application/json")
+        status = res.status_code
+        logger(status)
         return jsonify(output)
 
 
 @app.route("/api/v1/on-covid-19/xml", methods=["GET", "POST"])
 def xml_index():
+    g.start = time.time()
+
     if request.method == "GET":
-        return Response("", content_type="application/xml")
+        res = Response("", content_type="application/xml")
+        status = res.status[:3]
+        logger(status)
+        return res
 
     if request.method == "POST":
         data = request.get_json()
         output = estimator(data)
+        res = Response(dicttoxml(output, attr_type=False),  content_type="application/xml")
+        status = res.status[:3]
+        logger(status)
+        return res
 
-        return Response(dicttoxml(output, attr_type=False),  content_type="application/xml")
+
+@app.route("/api/v1/on-covid-19/logs", methods=["GET"])
+def logs_index():
+    with open(dir, "r") as f:
+        return "".join(f.readlines())
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
